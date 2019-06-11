82c9c4782ec54814370c81607608c67aac6b880e
fhfuih <sam.zyhuang@outlook.com>
2019-06-11 21:20:23 +0800

    Equal width ticks & smoothing adjustment

--- src/components/QuotaChart.js
+++ src/components/QuotaChart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import { withStyles } from '@material-ui/core/styles';
 
 import { ResponsiveContainer, ComposedChart, XAxis, YAxis, Legend, Line, Tooltip, Area } from 'recharts';
@@ -36,11 +37,11 @@ class QuotaChart extends Component {
                   <stop offset='95%' stopColor={pref.quotaChartStyle.avail} stopOpacity={0.2} />
                 </linearGradient>
               </defs>
-              <XAxis hide={false} type='number' scale='time' domain={[this.props.startTime, this.props.endTime]} dataKey='timestamp' tickFormatter={xAxisFmt} />
+              <XAxis hide={false} type='number' scale='time' domain={[this.props.startTime, this.props.endTime]} dataKey='timestamp' tickFormatter={xAxisFmt} ticks={this.props.ticks} />
               <YAxis />
-              {this.props.filter.quota && <Area dataKey='quota' type='basis' stroke={pref.quotaChartStyle.quota} fillOpacity={1} fill='url(#colorQuota)' />}
-              {this.props.filter.avail && <Area dataKey='avail' type='basis' stroke={pref.quotaChartStyle.avail} fillOpacity={1} fill='url(#colorAvail)' />}
-              {this.props.filter.wait && <Line dataKey='wait' type='basis' stroke={pref.quotaChartStyle.wait} dot={false} />}
+              {this.props.filter.quota && <Area dataKey='quota' type='monotoneX' stroke={pref.quotaChartStyle.quota} fillOpacity={1} fill='url(#colorQuota)' />}
+              {this.props.filter.avail && <Area dataKey='avail' type='monotoneX' stroke={pref.quotaChartStyle.avail} fillOpacity={1} fill='url(#colorAvail)' />}
+              {this.props.filter.wait && <Line dataKey='wait' type='monotoneX' stroke={pref.quotaChartStyle.wait} dot={false} />}
               <Legend />
               <Tooltip labelFormatter={legendLabelFmt} animationEasing='ease-out' />
             </ComposedChart>
@@ -52,13 +53,33 @@ class QuotaChart extends Component {
   }
 }
 
+function getDateArray(s, e) {
+  let a = [];
+  s = new Date(s);
+  e = new Date(e);
+  s.setDate(s.getDate() + 1);
+  s.setHours(0);
+  s.setMinutes(0);
+  s.setSeconds(0);
+  s.setMilliseconds(0);
+  while(s < e) {
+    a.push(new Date(s));
+    s.setDate(s.getDate() + 1);
+  }
+  return a;
+}
+const startTimeSelector = state => state.startTime;
+const endTimeSelector = state => state.endTime;
+const getTicks = createSelector([startTimeSelector, endTimeSelector], getDateArray);
+
 function mapStateToProps(state) {
   return {
     empty: Boolean(state.quota.length),
     startTime: state.startTime,
     endTime: state.endTime,
     data: state.quota,
-    filter: state.filter
+    filter: state.filter,
+    ticks: getTicks(state)
   }
 }
 
